226ab853bd75cd226ca8afbf378d1f5797b215c6
Discord Help <96002597+discordhelp20@users.noreply.github.com>
2022-01-02 08:13:36 +0530

    Create messageCreate.js

---
+++ 03 Handlers/events/messageCreate.js
@@ -0,0 +1,27 @@
+module.exports = {
+    run: (client, message) => {
+        if(message.author.bot || !message.guild || !message.content.startsWith(client.settings.prefix)) return;
+        const args = message.content.slice(client.settings.prefix.length).trim().split(/ +/g);
+        const cmd = args.shift().toLowerCase();
+
+        let command = client.pr_commands.get(cmd)
+        if (!command) command = client.pr_commands.get(client.aliases.get(cmd));
+        if(!command) return;
+
+        if (command.botPermissions) {
+            const Permissions = command.botPermissions.filter(x => !message.guild.me.permissions.has(x)).map(x => "`" + x + "`")
+            if (Permissions.length) return message.channel.send(`I need ${Permissions.join(", ")} permission(s) to execute the command!`)
+        } 
+          
+        if (command.memberPermissions) {
+            const Permissions = command.memberPermissions.filter(x => !message.member.permissions.has(x)).map(x => "`" + x + "`")
+            if (Permissions.length) return message.channel.send(`You need ${Permissions.join(", ")} permission(s) to execute this command!`)
+        }
+        
+        if (command.ownerOnly) {
+            if (message.author.id !== client.settings.ownerId) return message.channel.send("This command can only be use by owner :C")
+        }
+
+        command.run(client, message, args)
+    }
+              };
